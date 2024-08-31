c09afb23925f4686cc8668a5ae37443308a6ae3a
gregory1175 <151869985+gregory1175@users.noreply.github.com>
2024-08-31 03:40:42 +0300

    Add Player and Animation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import style from "./style.css";
 import { scense } from "./scenes";
 
 new Phaser.Game({
@@ -7,8 +6,8 @@ new Phaser.Game({
   height: 600,
   title: "MyRPG",
   scene: scense,
-  url: import.meta.env.URL || "",
-  version: import.meta.env.VERSION || "0.0.1",
+  url: /* import.meta.env.URL  || */ "",
+  version: /* import.meta.env.VERSION || */ "0.0.1",
   backgroundColor: "#000",
   scale: {
     mode: Phaser.Scale.FIT,
--- src/scenes/BaseWorld.ts
+++ src/scenes/BaseWorld.ts
@@ -1,1 +1,37 @@
-export class BaseWorld {}
+import tilesJSON from "../assets/BaseWorld.json";
+import { Player } from "../entity/player";
+import { SIZES, TILES, LAYERS, SPRITES } from "../utils/constants";
+
+export class BaseWorld extends Phaser.Scene {
+  private player?: Player;
+  constructor() {
+    super("MyRPG");
+  }
+
+  preload() {
+    this.load.image(TILES.BASEWORLD, "src/assets/Base.png");
+    this.load.tilemapTiledJSON("map", "src/assets/BaseWorld.json");
+    this.load.spritesheet(SPRITES.PLAYER, "src/assets/characters/Man.png", {
+      frameWidth: SIZES.PLAYER.WIDTH,
+      frameHeight: SIZES.PLAYER.HEIGHT,
+    });
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage(
+      tilesJSON.tilesets[0].name,
+      TILES.BASEWORLD,
+      SIZES.TILE,
+      SIZES.TILE
+    );
+    const groundLayer = map.createLayer(LAYERS.GROUND, tileset, 0, 0);
+    const wallsLayer = map.createLayer(LAYERS.WALLS, tileset, 0, 0);
+
+    this.player = new Player(this, 400, 250, SPRITES.PLAYER);
+  }
+
+  update(_: number, delta: number) {
+    this.player.update(delta);
+  }
+}
